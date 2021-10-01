d8352e4deb219f9c1ec66283f7a7500cf5ea33be
yibmmok <liwa0000@gmail.com>
2021-10-01 11:02:58 +0800

    init add all files

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,105 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <Header @toggle-add-task="toggleAddTask" title="排程追蹤器" :showAddTask="showAddTask" />
+    <div v-show="showAddTask">
+      <AddTask @add-task="addTask"/>
+    </div>
+    <Tasks @toggle-reminder="toggleReminder" @delete-task="deleteTask" :tasks="tasks"/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    Tasks,
+    AddTask
+  },
+  data() {
+    return {
+      tasks: [],
+      showAddTask: false
+    }
+  },
+  methods: {
+    toggleAddTask() {
+      this.showAddTask = !this.showAddTask
+    },
+    addTask(task) {
+      this.tasks = [...this.tasks, task]
+    },
+    deleteTask(id) {
+      if (confirm('是否刪除?')) {
+        this.tasks = this.tasks.filter((task) => task.id !== id)
+      }
+    },
+    toggleReminder(id) {
+      this.tasks = this.tasks.map(
+        (task) => task.id === id ? {...task, reminder: !task.reminder} : task
+      )
+    },
+    async fetchTasks() {
+      // const res = await fetch('http://localhost:5001/tasks')
+      const res = await fetch('http://localhost/api/test01/fetchTasks.php')
+
+      const data = await res.json()
+      console.log(data)
+      return data.tasks
+    }
+  },
+  async created() {
+    this.tasks = this.fetchTasks()
+    // this.tasks.push(this.fetchTasks())
+    console.log('Tasks = ', this.tasks)
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap');
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+  body {
+    font-family: 'Poppins', sans-serif;
+  }
+  .container {
+    max-width: 500px;
+    margin: 30px auto;
+    overflow: auto;
+    min-height: 300px;
+    border: 1px solid steelblue;
+    padding: 30px;
+    border-radius: 5px;
+  }
+  .btn {
+    display: inline-block;
+    background: #000;
+    color: #fff;
+    border: none;
+    padding: 10px 20px;
+    margin: 5px;
+    border-radius: 5px;
+    cursor: pointer;
+    text-decoration: none;
+    font-size: 15px;
+    font-family: inherit;
+  }
+  .btn:focus {
+    outline: none;
+  }
+  .btn:active {
+    transform: scale(0.98);
+  }
+  .btn-block {
+    display: block;
+    width: 100%;
+  }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+	<header>
+		<h1>{{ title }}</h1>
+		<Button 
+			@btn-click="$emit('toggle-add-task')" 
+			:text="showAddTask ? '關閉表單' : '新增排程'" 
+			:color="showAddTask ? 'red' : 'green'"
+		/>
+	</header>
+</template>
+
+<script>
+	import Button from './Button'
+
+	export default {
+		name: 'Header',
+		props: {
+			title: {
+				type: String,
+			},
+			showAddTask: Boolean,
+		},
+		components: {
+			Button
+		}
+	}
+</script>
+
+<style scoped>
+	header {
+		display: flex;
+		justify-content: space-between;
+		align-items:center;
+		margin-bottom: 20px;
+	}	
+</style>
